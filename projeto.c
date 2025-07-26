9722fe95f718c1fc18bb6d0a4a87f32d7fb25cfd
JoaoPedro <joaohulck@gmail.com>
2025-07-25 22:15:36 -0300

    Update

--- projeto.c
+++ projeto.c
@@ -61,3 +61,35 @@ void free_resources(game *game) {
   free(game->board);
   free(game);
 }
+
+int valid_move (game *game, unsigned int x, unsigned int y, tile *tile) {
+  //The borders must be 0-colored
+  if (x == 0 && W_COLOR(tile) != 0) return 0;
+  if (y == 0 && N_COLOR(tile) != 0) return 0;
+  if (x == game->size - 1 && E_COLOR(tile) != 0) return 0;
+  if (y == game->size - 1 && S_COLOR(tile) != 0) return 0;
+
+  //The tile must also be compatible with its existing neighbours
+  if (x > 0 && game->board[x - 1][y] != NULL &&
+      E_COLOR(game->board[x - 1][y]) != W_COLOR(tile))
+    return 0;
+  if (x < game->size - 1 && game->board[x + 1][y] != NULL &&
+      W_COLOR(game->board[x + 1][y]) != E_COLOR(tile))
+    return 0;
+  if (y > 0 && game->board[x][y - 1] != NULL &&
+      S_COLOR(game->board[x][y - 1]) != N_COLOR(tile))
+    return 0;
+  if (y < game->size - 1 && game->board[x][y + 1] != NULL &&
+      N_COLOR(game->board[x][y + 1]) != S_COLOR(tile))
+    return 0;
+
+  return 1;
+}
+
+void print_solution (game *game) {
+  for(unsigned int j = 0; j < game->size; j++)
+    for(unsigned int i = 0; i < game->size; i++) {
+      tile *t = game->board[i][j];
+      printf("%u %u\n", t->id, t->rotation);
+    }
+}
